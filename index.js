d98a58f2630839b49262f7ae973019590308711c
shawnmcclure <shawn850mcclure@gmail.com>
2023-09-21 21:09:32 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(want = "roller-skate"){
+    return (`This Saturday, I want to ${want}!`);
+}
+function mondayWork(dos = "go to the office"){
+    return (`This Monday, I will ${dos}.`)
+}
+function wrapAdjective(wrapper="*"){
+    return function (str){
+    return `You are ${wrapper}${str}${wrapper}!`
+    }
+}
